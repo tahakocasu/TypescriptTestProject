a76fab9308d138abd845dea1de25e0c9025a8921
taha.kocasu <tkocasu@gmail.com>
2022-08-29 21:32:04 +0300

    something changes

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,12 @@ let age = 20;
 if (age < 50)
     age += 10;
 console.log(age);
+let Players = {
+    id: 0,
+    name: 'abc',
+    retire: (props) => {
+        console.log(props);
+    }
+};
+Players.retire(Players.id.toString() + Players.name);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,22 @@ let age: number = 20;
 if (age < 50)
    age += 10;
 console.log(age)
+
+type Users = {
+   readonly id: number,
+   name: string,
+   retire: (props: string) => void
+}
+
+let Players: Users = {
+   id: 0,
+   name: 'abc',
+   retire: (props: string) => {
+      console.log(props)
+   }
+}
+Players.retire(Players.id.toString() + Players.name)
+
+const sendReq = (props: string | number): number => {
+   return typeof props === 'number' ? props * 2.2 : parseInt(props) * 2.2;
+}
